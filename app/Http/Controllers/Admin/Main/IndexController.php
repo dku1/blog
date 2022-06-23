e8d584af09f32a90d72bae631b664d090634e862
dku1 <denis.kulagin1593@mail.ru>
2022-06-23 17:33:42 +0700

    Start Front admin and client

---
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Main;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke(): Factory|View|Application
+    {
+        return view('admin.main.index');
+    }
+}
