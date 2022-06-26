42505262ba9b5aadafe62364b8fa9d7fa544f413
dku1 <denis.kulagin1593@mail.ru>
2022-06-26 13:58:47 +0700

    Store user through queue

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -4,24 +4,15 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
-use App\Mail\User\PasswordMail;
-use App\Models\User;
-use Illuminate\Auth\Events\Registered;
+use App\Jobs\StoreUserJob;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request): RedirectResponse
     {
         $data = $request->validated();
-        $password = Str::random(10);
-        $data['password'] = Hash::make($password);
-        $user = User::create($data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
-        event(new Registered($user));
+        StoreUserJob::dispatch($data);
         return redirect()->route('admin.users.index');
     }
 }
